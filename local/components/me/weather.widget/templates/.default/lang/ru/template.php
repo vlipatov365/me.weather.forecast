8c5ece1b61e65db56a6c43f250d5508b3a46a2ca
Vyacheslav Lipatov <vlipatov365@yandex.ru>
2025-06-11 16:56:17 +0300

    Revert "Модуль и компонент для получения погоды от стороннего сервиса"
    This reverts commit f600060f98bf98125e101dbdf19a6ab67743eef5.

--- local/components/me/weather.widget/templates/.default/lang/ru/template.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$MESS['WEATHER_WIDGET_ERROR'] = 'Ошибка при получении данных';
-$MESS['WEATHER_WIDGET_TEMPERATURE'] = 'Температура';
-$MESS['WEATHER_WIDGET_HUMIDITY'] = 'Влажность';
-$MESS['WEATHER_WIDGET_PRESSURE'] = 'Давление';
-$MESS['WEATHER_WIDGET_DATE'] = 'Дата';
-$MESS['WEATHER_WIDGET_MM_HG'] = 'мм рт. ст.';
--- local/modules/weather.forecast/lang/ru/lib/Service/Weather/WeatherFactory.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$MESS['WEATHER_FACTOR_ERR_WEATHER_NOT_FOUND'] = 'Сервис не найден';
